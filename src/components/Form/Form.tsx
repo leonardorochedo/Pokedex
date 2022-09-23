d0af06528f74c07fa4e3e83201d0b4e40a0f6c46
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-09-23 16:14:54 -0300

    fixind card

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Form() {
+    return (
+        <form action="">
+            <input type="text" placeholder='Digite o nome do Pokemon' />
+            <button>OK</button>
+        </form>
+    );
+}
--- src/components/PokeCard/PokeCard.tsx
+++ src/components/PokeCard/PokeCard.tsx
@@ -5,6 +5,7 @@ import './PokeCard.css'
 export function PokeCard() {
 
   const [pokemon, setPokemon] = useState({
+    img: '',
     name: '',
     id: 0,
     typeOne: '',
@@ -23,28 +24,54 @@ export function PokeCard() {
   .then((response) => response.json())
   .then((data) => {
     setPokemon({
-      name: data.forms[0].name,
-      id: data.id,
-      typeOne: data.types[0].type.name,
-      typeTwo: data.types[1].type.name,
-      height: data.height/10,
-      weight: data.weight/10,
-      hp: data.stats[0].base_stat,
-      atk: data.stats[1].base_stat,
-      def: data.stats[2].base_stat,
-      sa: data.stats[3].base_stat,
-      sd: data.stats[4].base_stat,
-      speed: data.stats[5].base_stat,
+        img: data.sprites.front_default,
+        name: data.forms[0].name,
+        id: data.id,
+        typeOne: data.types[0].type.name,
+        typeTwo: data.types[1].type.name,
+        height: data.height/10,
+        weight: data.weight/10,
+        hp: data.stats[0].base_stat,
+        atk: data.stats[1].base_stat,
+        def: data.stats[2].base_stat,
+        sa: data.stats[3].base_stat,
+        sd: data.stats[4].base_stat,
+        speed: data.stats[5].base_stat,
     })
   })
 
   return (
     <div className="container-info">
-      <h1>Name: {pokemon.name} ID #{pokemon.id}</h1>
-      <h1>Types : {pokemon.typeOne}, {pokemon.typeTwo}</h1>
-      <h1>Height: {pokemon.height}</h1>
-      <h1>Weight: {pokemon.weight}</h1>
-      <h1>Stats: HP{pokemon.hp} ATK{pokemon.atk} DEF{pokemon.def} SA{pokemon.sa} SD{pokemon.sd} SPEED{pokemon.speed}</h1>
+        <div className="poke-img">
+            <img src={pokemon.img} alt="Foto do Pokemon" />
+        </div>
+        <div className="name-id">
+            <p>{pokemon.name} #{pokemon.id}</p>
+        </div>
+        <div className="types">
+            <p>{pokemon.typeOne}</p>
+            <p>{pokemon.typeTwo}</p>
+        </div>
+        <div className="structure">
+            <div className="structure-height">
+                <p>{pokemon.height} KG</p>
+                <p>Height</p>
+            </div>
+            <div className="structure-weight">
+                <p>{pokemon.weight} M</p>
+                <p>Weight</p>
+            </div>
+        </div>
+        <div className="stats">
+            <ul>
+                <li><p>HP</p> <p>{pokemon.hp}</p></li>
+                <li><p>ATK</p> <p>{pokemon.atk}</p></li>
+                <li><p>DEF</p> <p>{pokemon.def}</p></li>
+                <li><p>SA</p> <p>{pokemon.sa}</p></li>
+                <li><p>SD</p> <p>{pokemon.sd}</p></li>
+                <li><p>SPEED</p> <p>{pokemon.speed}</p></li>
+            </ul>
+        </div>
     </div>
   )
 }
