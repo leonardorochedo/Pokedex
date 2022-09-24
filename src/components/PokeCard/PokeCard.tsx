6699cfb022050f28db6b3b8ef18582125c98ebcb
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2022-09-24 12:47:14 -0300

    add styles

--- src/components/PokeCard/PokeCard.tsx
+++ src/components/PokeCard/PokeCard.tsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 
+import { BsSearch } from 'react-icons/bs'
+import pokebola from '../../assets/pokebola.png';
+
 import './PokeCard.css'
 
 export function PokeCard() {
 
+  const [pokeName, setPokeName] = useState('')
   const [pokemon, setPokemon] = useState({
-    img: '',
-    name: '',
+    img: `${pokebola}`,
+    name: '???',
     id: 0,
-    typeOne: '',
-    typeTwo: '',
+    typeOne: '???',
+    typeTwo: '???',
     height: 0,
     weight: 0,
     hp: 0,
@@ -20,9 +24,10 @@ export function PokeCard() {
     speed: 0,
   })
 
-  fetch("https://pokeapi.co/api/v2/pokemon/charizard")
-  .then((response) => response.json())
-  .then((data) => {
+  function consultAPI() {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName.toLowerCase()}`)
+    .then((response) => response.json())
+    .then((data) => {
     setPokemon({
         img: data.sprites.front_default,
         name: data.forms[0].name,
@@ -39,8 +44,15 @@ export function PokeCard() {
         speed: data.stats[5].base_stat,
     })
   })
+  }
 
   return (
+    <>
+    <h1>Pokedéx <img id='pokebola' src={pokebola} alt="Pokebola" /></h1>
+    <div className="form">
+        <input autoFocus type="text" placeholder='Digite o Pokemon' onChange={(e) => setPokeName(e.target.value)} />
+        <button onClick={consultAPI}><span><BsSearch size={20}/></span></button>
+    </div>
     <div className="container-info">
         <div className="poke-img">
             <img src={pokemon.img} alt="Foto do Pokemon" />
@@ -54,24 +66,25 @@ export function PokeCard() {
         </div>
         <div className="structure">
             <div className="structure-height">
-                <p>{pokemon.height} KG</p>
-                <p>Height</p>
+                <p id='number'>{pokemon.height} KG</p>
+                <p id='description'>Height</p>
             </div>
             <div className="structure-weight">
-                <p>{pokemon.weight} M</p>
-                <p>Weight</p>
+                <p id='number'>{pokemon.weight} M</p>
+                <p id='description'>Weight</p>
             </div>
         </div>
         <div className="stats">
             <ul>
-                <li><p>HP</p> <p>{pokemon.hp}</p></li>
-                <li><p>ATK</p> <p>{pokemon.atk}</p></li>
-                <li><p>DEF</p> <p>{pokemon.def}</p></li>
-                <li><p>SA</p> <p>{pokemon.sa}</p></li>
-                <li><p>SD</p> <p>{pokemon.sd}</p></li>
-                <li><p>SPEED</p> <p>{pokemon.speed}</p></li>
+                <li><p id='hp'>HP</p> <p>{pokemon.hp}</p></li>
+                <li><p id='atk'>ATK</p> <p>{pokemon.atk}</p></li>
+                <li><p id='def'>DEF</p> <p>{pokemon.def}</p></li>
+                <li><p id='sa'>SA</p> <p>{pokemon.sa}</p></li>
+                <li><p id='sd'>SD</p> <p>{pokemon.sd}</p></li>
+                <li><p id='speed'>SPEED</p> <p>{pokemon.speed}</p></li>
             </ul>
         </div>
     </div>
+    </>
   )
 }
